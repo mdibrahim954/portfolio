adf153aed359ee890fae859669536318f450311c
naimbhuya <mdnaimmizi954@gmail.com>
2026-04-24 19:46:36 +0600

    protfolio update from leptop

---
+++ components/serverComp/supabase.ts
@@ -0,0 +1,9 @@
+// lib/supabase.js
+import { createClient  } from '@supabase/supabase-js'
+
+
+
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY as string
+)
